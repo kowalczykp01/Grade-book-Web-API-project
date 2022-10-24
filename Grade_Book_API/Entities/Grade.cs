d816f449b20dc3389fc670a6bf8eed5b6fad69c6
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2022-10-24 18:53:42 +0200

    Updating database

--- Grade_Book_API/Entities/Grade.cs
+++ Grade_Book_API/Entities/Grade.cs
@@ -9,6 +9,7 @@ namespace Grade_Book_API.Entities
     public class Grade
     {
         public int Id { get; set; }
+        public string Description { get; set; }
         public int Value { get; set; }
         public virtual Subject Subject { get; set; }
         public int StudentId { get; set; }
