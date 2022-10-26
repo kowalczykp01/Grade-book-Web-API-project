233238565429502d42bbdab88b8a40c525d6c661
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2022-10-26 14:51:54 +0200

    Adding initial seeder service and adding it and DbContext service to the container.

---
+++ Grade_Book_API/GradeBookSeeder.cs
@@ -0,0 +1,67 @@
+﻿using Grade_Book_API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Book_API
+{
+    public class GradeBookSeeder
+    {
+        private readonly GradeBookDbContext _dbContext;
+
+        public GradeBookSeeder(GradeBookDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Students.Any())
+                {
+                    var students = GetStudents();
+                    _dbContext.Students.AddRange(students);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Student> GetStudents()
+        {
+            var students = new List<Student>()
+            {
+                new Student()
+                {
+                    FirstName = "Paweł",
+                    Surname = "Kowalczyk",
+                    DegreeCourse = "Computer Science",
+                    YearOfStudies = 3,
+                    ContactEmail = "pawelkowalczyk@email.com",
+                    Subjects = new List<Subject>()
+                    {
+                        new Subject()
+                        {
+                            Name = "Numerical methods",
+                            Grades = new List<Grade>()
+                            {
+                                new Grade()
+                                {
+                                    Description = "Exam",
+                                    Value = 10
+                                },
+                                new Grade()
+                                {
+                                    Description = "Test",
+                                    Value = 57
+                                }
+                            }
+                        }               
+                    }
+                }
+            };
+            return students;
+        }
+    }
+}
--- Grade_Book_API/Program.cs
+++ Grade_Book_API/Program.cs
@@ -1,14 +1,31 @@
+using Grade_Book_API;
+using Grade_Book_API.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<GradeBookDbContext>();
+builder.Services.AddScoped<GradeBookSeeder>();
 
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<GradeBookSeeder>();
+
+seeder.Seed();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
