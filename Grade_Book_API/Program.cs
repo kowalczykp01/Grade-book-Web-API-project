17ea8d0b6eb87bf722ec7aa4436ccd1a2b997c31
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2023-05-30 20:52:39 +0200

    Added SubjectService to services in Program.cs

--- Grade_Book_API/Program.cs
+++ Grade_Book_API/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<GradeBookDbContext>();
 builder.Services.AddScoped<GradeBookSeeder>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<ISubjectService, SubjectService>();
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
 builder.Services.AddScoped<RequestTimeMiddleware>();
 builder.Services.AddSwaggerGen();
