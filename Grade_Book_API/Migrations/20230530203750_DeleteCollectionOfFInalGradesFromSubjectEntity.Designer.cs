8a22b5af6c9fd6e47c51f161305f64d7c2ceb1c2
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2023-05-31 23:57:29 +0200

    Changes in database - deleted collection of FinalGrade from Subject

--- Grade_Book_API/Migrations/20230530203750_DeleteCollectionOfFInalGradesFromSubjectEntity.Designer.cs
+++ Grade_Book_API/Migrations/20230530203750_DeleteCollectionOfFInalGradesFromSubjectEntity.Designer.cs
@@ -4,6 +4,7 @@ using Grade_Book_API.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Grade_Book_API.Migrations
 {
     [DbContext(typeof(GradeBookDbContext))]
-    partial class GradeBookDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230530203750_DeleteCollectionOfFInalGradesFromSubjectEntity")]
+    partial class DeleteCollectionOfFInalGradesFromSubjectEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
