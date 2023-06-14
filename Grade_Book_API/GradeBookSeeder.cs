a8f92b64f06f2c720588ab5944ed36cd31b47c29
Paweł Kowalczyk <pawelkowalczyk2001@gmail.com>
2023-06-14 16:28:10 +0200

    Changed implementation of seeder

--- Grade_Book_API/GradeBookSeeder.cs
+++ Grade_Book_API/GradeBookSeeder.cs
@@ -1,4 +1,5 @@
 ﻿using Grade_Book_API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,10 +12,12 @@ namespace Grade_Book_API
     public class GradeBookSeeder
     {
         private readonly GradeBookDbContext _dbContext;
+        private readonly IPasswordHasher<Student> _passwordHasher;
 
-        public GradeBookSeeder(GradeBookDbContext dbContext)
+        public GradeBookSeeder(GradeBookDbContext dbContext, IPasswordHasher<Student> passwordHasher)
         {
             _dbContext = dbContext;
+            _passwordHasher = passwordHasher;
         }
         public void Seed()
         {
@@ -30,36 +33,68 @@ namespace Grade_Book_API
                 {
                     var subjects = new List<Subject>
                     {
-                        new Subject { Name = "Mathematics" },
-                        new Subject { Name = "Physics" },
-                        new Subject { Name = "Chemistry" }
+                        new Subject { Name = "Analiza matematyczna 2" },
+                        new Subject { Name = "Podstawy kryptografii" },
+                        new Subject { Name = "Metody numeryczne" }
                     };
 
+                    var anotherSubjects = new List<Subject>
+                    {
+                        new Subject { Name = "RPiS"}
+                    };
+
+                    anotherSubjects.Add(subjects.ElementAt(1));
+
                     _dbContext.Subjects.AddRange(subjects);
+                    _dbContext.Subjects.AddRange(anotherSubjects);
                     _dbContext.SaveChanges();
 
                     var student = new Student
                     {
-                        FirstName = "John",
-                        Surname = "Doe",
-                        DegreeCourse = "Computer Science",
-                        YearOfStudies = 3,
-                        ContactEmail = "john.doe@example.com",
-                        PasswordHash = "zahaszowanehaslo",
+                        FirstName = "Michał",
+                        Surname = "Radzewicz",
+                        DegreeCourse = "Informatyka",
+                        YearOfStudies = 1,
+                        ContactEmail = "s123456@sggw.edu.pl",
                         RoleId = 1,
                         Subjects = subjects
                     };
 
+                    var hashedPassword = _passwordHasher.HashPassword(student, "haslo:mradzewicz:123");
+                    student.PasswordHash = hashedPassword;
+
+                    var anotherStudent = new Student
+                    {
+                        FirstName = "Paweł",
+                        Surname = "Kowalczyk",
+                        DegreeCourse = "Law",
+                        YearOfStudies = 2,
+                        ContactEmail = "s098765@sggw.edu.pl",
+                        RoleId = 1,
+                        Subjects = anotherSubjects
+                    };
+
+                    var anotherHashedPassword = _passwordHasher.HashPassword(anotherStudent, "haslo:pkowalczyk:123");
+                    anotherStudent.PasswordHash = anotherHashedPassword;
+
                     var grades = new List<Grade>
                     {
-                        new Grade { DateOfIssue = DateTime.Now, Description = "Grade 1", Value = 90, Subject = subjects.ElementAt(0), Student = student },
-                        new Grade { DateOfIssue = DateTime.Now, Description = "Grade 2", Value = 80, Subject = subjects.ElementAt(0), Student = student },
-                        new Grade { DateOfIssue = DateTime.Now, Description = "Grade 3", Value = 95, Subject = subjects.ElementAt(1), Student = student }
+                        new Grade { DateOfIssue = DateTime.Now, Description = "Wejściówka", Value = 90, Subject = subjects.ElementAt(0), Student = student },
+                        new Grade { DateOfIssue = DateTime.Now, Description = "Projekt", Value = 80, Subject = subjects.ElementAt(0), Student = student },
+                        new Grade { DateOfIssue = DateTime.Now, Description = "Kolokwium I", Value = 95, Subject = subjects.ElementAt(1), Student = student }
+                    };
+
+                    var anotherGrades = new List<Grade>
+                    {
+                        new Grade { DateOfIssue = DateTime.Now, Description = "Egzamin", Value = 80, Subject = anotherSubjects.ElementAt(0), Student = anotherStudent },
+                        new Grade { DateOfIssue = DateTime.Now, Description = "Kolokwium", Value = 95, Subject = anotherSubjects.ElementAt(1), Student = anotherStudent }
                     };
 
                     student.Grades = grades;
+                    anotherStudent.Grades = anotherGrades;
 
                     _dbContext.Students.Add(student);
+                    _dbContext.Students.Add(anotherStudent);
                     _dbContext.SaveChanges();
                 }
                 if (!_dbContext.Roles.Any())
